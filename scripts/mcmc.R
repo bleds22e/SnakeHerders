f475980369a0d539b4ca507edfedf1b244320748
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2022-04-27 15:42:34 -0700

    move correlation_matrix function, which produces table with significance values, into it's own function also, use rcorr function

--- scripts/mcmc.R
+++ scripts/mcmc.R
@@ -109,102 +109,14 @@ RVAideMemoire::fisher.multcomp(x = seq13_matrix)
 rstatix::fisher_test(seq13_matrix, simulate.p.value = TRUE)
 rstatix::row_wise_fisher_test(seq13_matrix, simulate.p.values=TRUE)
 
+## all of these produce values that are not 1 along the diagonal, which I don't
+# really understand?
+
 library(corrplot)
 corrplot(FT_res_matrix, is.corr = FALSE)
 
-correlation_matrix
-
-#' correlation_matrix
-#' Creates a publication-ready / formatted correlation matrix, using `Hmisc::rcorr` in the backend.
-#'
-#' @param df dataframe; containing numeric and/or logical columns to calculate correlations for
-#' @param type character; specifies the type of correlations to compute; gets passed to `Hmisc::rcorr`; options are `"pearson"` or `"spearman"`; defaults to `"pearson"`
-#' @param digits integer/double; number of decimals to show in the correlation matrix; gets passed to `formatC`; defaults to `3`
-#' @param decimal.mark character; which decimal.mark to use; gets passed to `formatC`; defaults to `.`
-#' @param use character; which part of the correlation matrix to display; options are `"all"`, `"upper"`, `"lower"`; defaults to `"all"`
-#' @param show_significance boolean; whether to add `*` to represent the significance levels for the correlations; defaults to `TRUE`
-#' @param replace_diagonal boolean; whether to replace the correlations on the diagonal; defaults to `FALSE`
-#' @param replacement character; what to replace the diagonal and/or upper/lower triangles with; defaults to `""` (empty string)
-#'
-#' @return a correlation matrix
-#' @export
-#'
-#' @examples
-#' `correlation_matrix(iris)`
-#' `correlation_matrix(mtcars)`
-correlation_matrix <- function(df, 
-                               type = "pearson",
-                               digits = 3, 
-                               decimal.mark = ".",
-                               use = "all", 
-                               show_significance = TRUE, 
-                               replace_diagonal = FALSE, 
-                               replacement = ""){
-  
-  # check arguments
-  stopifnot({
-    is.numeric(digits)
-    digits >= 0
-    use %in% c("all", "upper", "lower")
-    is.logical(replace_diagonal)
-    is.logical(show_significance)
-    is.character(replacement)
-  })
-  # we need the Hmisc package for this
-  require(Hmisc)
-  
-  # retain only numeric and boolean columns
-  isNumericOrBoolean = vapply(df, function(x) is.numeric(x) | is.logical(x), logical(1))
-  if (sum(!isNumericOrBoolean) > 0) {
-    cat('Dropping non-numeric/-boolean column(s):', paste(names(isNumericOrBoolean)[!isNumericOrBoolean], collapse = ', '), '\n\n')
-  }
-  df = df[isNumericOrBoolean]
-  
-  # transform input data frame to matrix
-  x <- as.matrix(df)
-  
-  # run correlation analysis using Hmisc package
-  correlation_matrix <- Hmisc::rcorr(x, type = type)
-  R <- correlation_matrix$r # Matrix of correlation coeficients
-  p <- correlation_matrix$P # Matrix of p-value 
-  
-  # transform correlations to specific character format
-  Rformatted = formatC(R, format = 'f', digits = digits, decimal.mark = decimal.mark)
-  
-  # if there are any negative numbers, we want to put a space before the positives to align all
-  if (sum(!is.na(R) & R < 0) > 0) {
-    Rformatted = ifelse(!is.na(R) & R > 0, paste0(" ", Rformatted), Rformatted)
-  }
-  
-  # add significance levels if desired
-  if (show_significance) {
-    # define notions for significance levels; spacing is important.
-    stars <- ifelse(is.na(p), "", ifelse(p < .001, "***", ifelse(p < .01, "**", ifelse(p < .05, "*", ""))))
-    Rformatted = paste0(Rformatted, stars)
-  }
-  
-  # make all character strings equally long
-  max_length = max(nchar(Rformatted))
-  Rformatted = vapply(Rformatted, function(x) {
-    current_length = nchar(x)
-    difference = max_length - current_length
-    return(paste0(x, paste(rep(" ", difference), collapse = ''), sep = ''))
-  }, FUN.VALUE = character(1))
-  
-  # build a new matrix that includes the formatted correlations and their significance stars
-  Rnew <- matrix(Rformatted, ncol = ncol(x))
-  rownames(Rnew) <- colnames(Rnew) <- colnames(x)
-  
-  # replace undesired values
-  if (use == 'upper') {
-    Rnew[lower.tri(Rnew, diag = replace_diagonal)] <- replacement
-  } else if (use == 'lower') {
-    Rnew[upper.tri(Rnew, diag = replace_diagonal)] <- replacement
-  } else if (replace_diagonal) {
-    diag(Rnew) <- replacement
-  }
-  
-  return(Rnew)
-}
-
+source("scripts/correlation_matrix_fxn.R")
 correlation_matrix(as.data.frame(seq13_matrix))
+
+rcorr(seq13_matrix, type = "pearson")
+rcorr(FT_res_matrix, type = "pearson")
