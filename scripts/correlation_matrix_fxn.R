847edb94d7e0871bbc68e5d774447ecfa6672a0b
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2022-05-26 13:28:05 -0700

    add credits and a function
    give credit to the originator of these functions and link to his blog
    
    he also has a function for saving the output into a csv to easy copy into a word doc (basically just writing to csv, but I like it and want to give him credit

--- scripts/correlation_matrix_fxn.R
+++ scripts/correlation_matrix_fxn.R
@@ -1,3 +1,7 @@
+# These functions are taken from scripts written by Paul van der Laken.
+# You can find them here: https://paulvanderlaken.com/2020/07/28/publication-ready-correlation-matrix-significance-r/#save_correlation_matrix
+
+
 #' correlation_matrix
 #' Creates a publication-ready / formatted correlation matrix, using `Hmisc::rcorr` in the backend.
 #'
@@ -89,4 +93,20 @@ correlation_matrix <- function(df,
   }
   
   return(Rnew)
+}
+
+
+#' save_correlation_matrix
+#' Creates and save to file a fully formatted correlation matrix, using `correlation_matrix` and `Hmisc::rcorr` in the backend
+#' @param df dataframe; passed to `correlation_matrix`
+#' @param filename either a character string naming a file or a connection open for writing. "" indicates output to the console; passed to `write.csv`
+#' @param ... any other arguments passed to `correlation_matrix`
+#'
+#' @return NULL
+#'
+#' @examples
+#' `save_correlation_matrix(df = iris, filename = 'iris-correlation-matrix.csv')`
+#' `save_correlation_matrix(df = mtcars, filename = 'mtcars-correlation-matrix.csv', digits = 3, use = 'lower')`
+save_correlation_matrix = function(df, filename, ...) {
+  return(write.csv2(correlation_matrix(df, ...), file = filename))
 }
