8157479ec4cbed2b3aa79b801e2855639e7298af
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2022-05-26 13:44:28 -0700

    clean up script
    this commit includes:
    * sourcing the correlation_matrix_fxn
    * including videoID column and ensuring action sequences crossing two different videos are not included in the analyses
    * creating and saving a cleaned up correlation plot
    * creating and saving a correlation significance table

--- scripts/analyses.R
+++ scripts/analyses.R
@@ -13,6 +13,9 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, markovchain, kequate, Hmisc, corrplot)
 
+# Correlation Table Functions #
+source("scripts/correlation_matrix_fxn.R")
+
 # Data #
 
 # load ethogram sequence data from JLV
@@ -20,16 +23,22 @@ seq <- read_csv("data/JLV_seq.csv")
 
 # DATA CLEANING ------------------------------------------------------------####
 
-# clean up ethogram tags and states
-sequence <- seq %>% dplyr::select(ethogram_tag = `Ethogram Tag...10`) %>% 
-  mutate(across(where(is.character), ~ na_if(.,"")))
+# Clean up ethogram tags and states #
 
+# get required columns, replace blanks with NA, and fill in videoID values
+sequence <- seq %>% dplyr::select(videoID = `Video ID`, # will need later
+                                  ethogram_tag = `Ethogram Tag...10`) %>% 
+  mutate(across(where(is.character), ~ na_if(.,""))) %>% 
+  fill(videoID, .direction = "down")
+
+# standardize ethogram_tags
 sequence <- sequence %>% 
   mutate(ethogram_tag = replace(ethogram_tag, ethogram_tag == "Rocking +Sniffing", "Rocking + Sniffing"),
          ethogram_tag = replace(ethogram_tag, ethogram_tag == "Jump Yip", "Jump yip"),
          ethogram_tag = replace(ethogram_tag, ethogram_tag == "Moving away", "Retreat"),
          ethogram_tag = replace(ethogram_tag, ethogram_tag == "Moving toward", "Approach"))
 
+# create df with numbers for each ethogram_tag
 states <- tibble(number = seq(1:22),
                  ethogram_tag = c("Posting", "Jump yip", "Approach", "Scratching",
                                   "Pushing dirt", "Rocking", "Sniffing", 
@@ -39,20 +48,28 @@ states <- tibble(number = seq(1:22),
                                   "Vocalizing", "Head to Head", "Fake foraging",
                                   "Observing", "Greet-kiss", "Running"))
 
-# remove NAs and any doubles
+# merge df and remove NAs from ethogram_tag or number columns
 sequence <- full_join(sequence, states) %>% 
-  drop_na() %>% 
+  drop_na(ethogram_tag, number)
+
+# Given the way the sequence data were entered, the last action from one obs and
+# the first action from the next would be included in the matrix. By adding an
+# NA back in at the start of every video, those action pairs will not be included
+# in the the matrix.
+
+sequence <- sequence %>% 
+  group_split(videoID) %>%      # create multiple dfs based on videoID column
+  map_dfr(~ .x %>% add_row())   # add a row with NAs to the end of each dataframe 
+                                # the map_dfr function then recombines into one df
+
+# remove any doubled behaviors
+sequence <- sequence %>%  
   filter(ethogram_tag != lag(ethogram_tag))
 
-# reduce to 13 states and remove any new doubles created
+# reduce to 13 key states and remove any new doubles created
 sequence13 <- sequence %>% 
   filter(number <= 13) %>% 
   filter(ethogram_tag != lag(ethogram_tag))
-
-# need to remove counts that are jumping from one observation to the next ###
-
-
-
 
 
 # TEST MARKOV PROPERTY ------------------------------------------------------####
@@ -69,33 +86,45 @@ seq13_matrix <- createSequenceMatrix(stringchar = as.vector(sequence13$ethogram_
 
 # Test Markov Property #
 
-# need to convert to character matrix
-seq22_matrix_char <- as.character(seq22_matrix)
-mc_fit_22 <- markovchainFit(data = seq22_matrix_char)
-verifyMarkovProperty(seq22_matrix_char)
+# full matrix
+seq22_matrix_char <- as.character(seq22_matrix) # convert to character matrix
+markov_prop22 <- (verifyMarkovProperty(seq22_matrix_char))
 
-# is significant, meaning is non-random
+# reduced matrix
 seq13_matrix_char <- as.character(seq13_matrix)
-mc_fit_13 <- markovchainFit(seq13_matrix_char)
-verifyMarkovProperty(seq13_matrix_char)
+markov_prop13 <- (verifyMarkovProperty(seq13_matrix_char))
 
 
 # TESTING RESIDUALS --------------------------------------------------------####
 
-# run chi-sq test to get expected values matrix
-chi_sq <- chisq.test(seq13_matrix)
-chi_sq$expected
+# Focusing on reduced matrix #
 
-# attempt FTres() from kequate
-FT_residuals <- FTres(seq13_matrix, chi_sq$expected)
-FT_res_matrix <- matrix(FT_residuals, nrow = 13, ncol = 13)
+# run chi-sq test 
+chi_sq <- chisq.test(seq13_matrix, simulate.p.value = TRUE)
+chi_sq
 
-# Get correlations
-rcorr_seq13 <- rcorr(seq13_matrix, type = "pearson")
+# get expected values to compare to observed
+expected <- chi_sq$expected
+
+# get Freeman-Tukey residuals (observed vs. expected values)
+FT_res <- kequate::FTres(seq13_matrix, expected)
+
+# turn into a matrix of FT values
+FT_res_matrix <- matrix(FT_res, nrow = 13, ncol = 13)
+rownames(FT_res_matrix) <- rownames(seq13_matrix)
+colnames(FT_res_matrix) <- colnames(seq13_matrix)
+
+# get correlations
 rcorr_FTres <- rcorr(FT_res_matrix, type = "pearson")
 
-# Plot Correlation Plots
-corrplot(rcorr_FTres$r, is.corr = TRUE, diag = FALSE, type = "upper")
-corrplot(rcorr_FTres$P)
-corrplot(rcorr_seq13$r, diag = FALSE, type = "lower")
+# plot correlation
+dev.copy(png, "plots/FT_residuals.png")
+corrplot(rcorr_FTres$r, is.corr = TRUE, diag = FALSE, 
+         type = "upper", tl.col = "black")
+dev.off()
 
+# pull out significance values
+corr_matrix <- correlation_matrix(as.data.frame(FT_res_matrix), replace_diagonal = )
+save_correlation_matrix(as.data.frame(FT_res_matrix),
+                        "data/correlation_matrix_FTres.csv",
+                        digits = 3, use = "lower", replace_diagonal = TRUE)
